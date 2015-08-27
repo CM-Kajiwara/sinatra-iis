b183caab915fa3749e4d28365f324c24ef44618b
KAJIWARA Yutaka <kajiwara.yutaka@classmethod.jp>
2015-08-27 10:09:23 +0900

    First Commit

---
+++ server.rb
@@ -0,0 +1,27 @@
+require 'bundler/setup'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+set :public_folder, File.dirname(__FILE__) + '/static'
+get '/' do
+  erb :index
+end
+
+# アップロード処理
+post '/upload' do
+  if params[:file] && params[:execution]
+    if params[:execution].to_s =="true" 
+      save_path = "./executionStored/#{params[:file][:filename]}"
+    elsif  params[:execution].to_s == "false"
+      save_path = "./nonExecutionStoreed/#{params[:file][:filename]}"
+    end
+    p save_path
+
+    File.open(save_path, 'wb') do |f|
+      p params[:file][:tempfile]
+      f.write params[:file][:tempfile].read
+      @mes = "アップロード成功"
+    end
+  else
+    @mes = "アップロード失敗"
+  end
+end
